af6aac962f0ddcbccf815dff6f14a9292c9df62a
LeMoussel <cnhx27@gmail.com>
2020-10-19 17:00:13 +0200

    Initial commit

--- settings.py
+++ settings.py
@@ -4,11 +4,11 @@ ROBOTSTXT_OBEY = True
 CONCURRENT_REQUESTS = 5
 # Maximum navigation time in milliseconds
 PLAYWRIGHT_NAVIGATION_TIMEOUT = 30000
-# Browser type (chromium, firefox, webkit) created when Playwright connects to a browser instance
-PLAYWRIGHT_BROWSER_TYPE = 'chromium'
+# Browser type created when Playwright connects to a browser instance
+PLAYWRIGHT_BROWSER_TYPE = 'chromium' # firefox, webkit
 # Set of configurable options to set on the browser.
 # See https://github.com/microsoft/playwright/blob/master/docs/api.md#browsertypelaunchoptions for description fields
 PLAYWRIGHT_LAUNCH_OPTIONS = {
   'headless': False,
-  'executablePath': 'C:/Program Files/Google/Chrome/Application/chrome.exe',
+  'executablePath': 'C:/Program Files (x86)/Google/Chrome/Application/chrome.exe',
 }
